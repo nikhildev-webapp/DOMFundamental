6eb8186cd1f07e2b06df55c9ef0da63adb5cd6b9
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-11 16:11:10 +0530

    form update

--- formValidation.js
+++ formValidation.js
@@ -1,10 +1,19 @@
+const { createElement } = require("react");
+
 const ValidateForm = () => {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     let errMsg = document.getElementById('errorMsg');
     
     if (username === "" && password === '') {
-        alert('Enter the '); 
+        alert('Fill the form'); 
+    } else if (username !== 'nikhilkholiya' && password === 'helloworld') {
+        let alert = createElement(p);
+        alert.textContent = 'userName and password not matched';
+        alert.style.color = 'red';
+        errMsg.appendChild(alert);
+    } else {
+        alert('Login successfully')
     }
     
     
